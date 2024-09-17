9f799bd13f2bbbbb68ec6f37ba3d8f2b8800e3b2
mieko-c <150801549+mieko-c@users.noreply.github.com>
2024-09-17 17:42:24 +0900

    修正

--- js/thanks.js
+++ js/thanks.js
@@ -1,12 +1,23 @@
 // scotch display,scotch text,しっぽり明朝---
-(function(d) {
-    var config = {
-      kitId: 'iim6jay',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-  })(document);
+// (function(d) {
+//     var config = {
+//       kitId: 'iim6jay',
+//       scriptTimeout: 3000,
+//       async: true
+//     },
+//     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+//   })(document);
+//---
+
+// scotch display,しっぽり明朝必要ウェイトのみ抜粋---
+  // (function(d) {
+  //   var config = {
+  //     kitId: 'iim6jay',
+  //     scriptTimeout: 3000,
+  //     async: true
+  //   },
+  //   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+  // })(document);
 //---
 
 // accent-color変化---
